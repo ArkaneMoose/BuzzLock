bf5fe7d245c1fa56bbfdf6d11f3165fe936d7c82
Josh Shafran <j.a.shafran@gmail.com>
2020-04-22 12:22:12 -0400

    Autogenerated layout changes

--- BuzzLockGui/BuzzLockGui/FormOptions.Designer.cs
+++ BuzzLockGui/BuzzLockGui/FormOptions.Designer.cs
@@ -56,10 +56,11 @@
             // 
             // btnOptionsSave
             // 
-            this.btnOptionsSave.Location = new System.Drawing.Point(653, 235);
+            this.btnOptionsSave.Location = new System.Drawing.Point(1306, 452);
+            this.btnOptionsSave.Margin = new System.Windows.Forms.Padding(6);
             this.btnOptionsSave.Name = "btnOptionsSave";
             this.btnOptionsSave.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnOptionsSave.Size = new System.Drawing.Size(119, 32);
+            this.btnOptionsSave.Size = new System.Drawing.Size(238, 62);
             this.btnOptionsSave.TabIndex = 4;
             this.btnOptionsSave.Text = "Save";
             this.btnOptionsSave.UseVisualStyleBackColor = true;
@@ -67,10 +68,11 @@
             // 
             // btnEditProfile
             // 
-            this.btnEditProfile.Location = new System.Drawing.Point(12, 121);
+            this.btnEditProfile.Location = new System.Drawing.Point(24, 233);
+            this.btnEditProfile.Margin = new System.Windows.Forms.Padding(6);
             this.btnEditProfile.Name = "btnEditProfile";
             this.btnEditProfile.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnEditProfile.Size = new System.Drawing.Size(119, 32);
+            this.btnEditProfile.Size = new System.Drawing.Size(238, 62);
             this.btnEditProfile.TabIndex = 5;
             this.btnEditProfile.Text = "Profile";
             this.btnEditProfile.UseVisualStyleBackColor = true;
@@ -78,10 +80,11 @@
             // 
             // btnEditAuth
             // 
-            this.btnEditAuth.Location = new System.Drawing.Point(12, 159);
+            this.btnEditAuth.Location = new System.Drawing.Point(24, 306);
+            this.btnEditAuth.Margin = new System.Windows.Forms.Padding(6);
             this.btnEditAuth.Name = "btnEditAuth";
             this.btnEditAuth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnEditAuth.Size = new System.Drawing.Size(119, 32);
+            this.btnEditAuth.Size = new System.Drawing.Size(238, 62);
             this.btnEditAuth.TabIndex = 6;
             this.btnEditAuth.Text = "Authentication";
             this.btnEditAuth.UseVisualStyleBackColor = true;
@@ -89,10 +92,11 @@
             // 
             // btnRemoveUser
             // 
-            this.btnRemoveUser.Location = new System.Drawing.Point(12, 197);
+            this.btnRemoveUser.Location = new System.Drawing.Point(24, 379);
+            this.btnRemoveUser.Margin = new System.Windows.Forms.Padding(6);
             this.btnRemoveUser.Name = "btnRemoveUser";
             this.btnRemoveUser.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnRemoveUser.Size = new System.Drawing.Size(119, 32);
+            this.btnRemoveUser.Size = new System.Drawing.Size(238, 62);
             this.btnRemoveUser.TabIndex = 7;
             this.btnRemoveUser.Text = "Remove user";
             this.btnRemoveUser.UseVisualStyleBackColor = true;
@@ -101,27 +105,30 @@
             // txtEditProfile
             // 
             this.txtEditProfile.AutoSize = true;
-            this.txtEditProfile.Location = new System.Drawing.Point(137, 131);
+            this.txtEditProfile.Location = new System.Drawing.Point(274, 252);
+            this.txtEditProfile.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtEditProfile.Name = "txtEditProfile";
-            this.txtEditProfile.Size = new System.Drawing.Size(128, 13);
+            this.txtEditProfile.Size = new System.Drawing.Size(257, 25);
             this.txtEditProfile.TabIndex = 8;
             this.txtEditProfile.Text = "Edit name, phone, picture";
             // 
             // txtEditAuth
             // 
             this.txtEditAuth.AutoSize = true;
-            this.txtEditAuth.Location = new System.Drawing.Point(137, 169);
+            this.txtEditAuth.Location = new System.Drawing.Point(274, 325);
+            this.txtEditAuth.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtEditAuth.Name = "txtEditAuth";
-            this.txtEditAuth.Size = new System.Drawing.Size(161, 13);
+            this.txtEditAuth.Size = new System.Drawing.Size(326, 25);
             this.txtEditAuth.TabIndex = 9;
             this.txtEditAuth.Text = "Edit your authentication methods";
             // 
             // txtRemoveUser
             // 
             this.txtRemoveUser.AutoSize = true;
-            this.txtRemoveUser.Location = new System.Drawing.Point(137, 207);
+            this.txtRemoveUser.Location = new System.Drawing.Point(274, 398);
+            this.txtRemoveUser.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtRemoveUser.Name = "txtRemoveUser";
-            this.txtRemoveUser.Size = new System.Drawing.Size(185, 13);
+            this.txtRemoveUser.Size = new System.Drawing.Size(375, 25);
             this.txtRemoveUser.TabIndex = 10;
             this.txtRemoveUser.Text = "Permanently delete your user account";
             // 
@@ -129,9 +136,10 @@
             // 
             this.tbxStatus.AutoSize = true;
             this.tbxStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F);
-            this.tbxStatus.Location = new System.Drawing.Point(12, 9);
+            this.tbxStatus.Location = new System.Drawing.Point(24, 17);
+            this.tbxStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.tbxStatus.Name = "tbxStatus";
-            this.tbxStatus.Size = new System.Drawing.Size(190, 26);
+            this.tbxStatus.Size = new System.Drawing.Size(379, 52);
             this.tbxStatus.TabIndex = 11;
             this.tbxStatus.Text = "Welcome, <user>.";
             // 
@@ -139,18 +147,20 @@
             // 
             this.txtOptionsTitle.AutoSize = true;
             this.txtOptionsTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F);
-            this.txtOptionsTitle.Location = new System.Drawing.Point(12, 83);
+            this.txtOptionsTitle.Location = new System.Drawing.Point(24, 160);
+            this.txtOptionsTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtOptionsTitle.Name = "txtOptionsTitle";
-            this.txtOptionsTitle.Size = new System.Drawing.Size(248, 26);
+            this.txtOptionsTitle.Size = new System.Drawing.Size(495, 52);
             this.txtOptionsTitle.TabIndex = 12;
             this.txtOptionsTitle.Text = "BuzzLock Options Menu";
             // 
             // txtUserPermission
             // 
             this.txtUserPermission.AutoSize = true;
-            this.txtUserPermission.Location = new System.Drawing.Point(15, 39);
+            this.txtUserPermission.Location = new System.Drawing.Point(30, 75);
+            this.txtUserPermission.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtUserPermission.Name = "txtUserPermission";
-            this.txtUserPermission.Size = new System.Drawing.Size(104, 13);
+            this.txtUserPermission.Size = new System.Drawing.Size(212, 25);
             this.txtUserPermission.TabIndex = 13;
             this.txtUserPermission.Text = "Permission Level: <>";
             // 
@@ -163,10 +173,10 @@
             // 
             this.txtOptionsStatus.AutoSize = true;
             this.txtOptionsStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOptionsStatus.Location = new System.Drawing.Point(15, 61);
-            this.txtOptionsStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtOptionsStatus.Location = new System.Drawing.Point(30, 117);
+            this.txtOptionsStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtOptionsStatus.Name = "txtOptionsStatus";
-            this.txtOptionsStatus.Size = new System.Drawing.Size(165, 17);
+            this.txtOptionsStatus.Size = new System.Drawing.Size(314, 31);
             this.txtOptionsStatus.TabIndex = 14;
             this.txtOptionsStatus.Text = "30 seconds until timeout.";
             // 
@@ -178,9 +188,10 @@
             // txtCurrentName
             // 
             this.txtCurrentName.AutoSize = true;
-            this.txtCurrentName.Location = new System.Drawing.Point(15, 132);
+            this.txtCurrentName.Location = new System.Drawing.Point(30, 254);
+            this.txtCurrentName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtCurrentName.Name = "txtCurrentName";
-            this.txtCurrentName.Size = new System.Drawing.Size(73, 13);
+            this.txtCurrentName.Size = new System.Drawing.Size(148, 25);
             this.txtCurrentName.TabIndex = 15;
             this.txtCurrentName.Text = "Current name:";
             this.txtCurrentName.Click += new System.EventHandler(this.txtCurrentName_Click);
@@ -188,69 +199,77 @@
             // txtNewName
             // 
             this.txtNewName.AutoSize = true;
-            this.txtNewName.Location = new System.Drawing.Point(237, 132);
+            this.txtNewName.Location = new System.Drawing.Point(474, 254);
+            this.txtNewName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtNewName.Name = "txtNewName";
-            this.txtNewName.Size = new System.Drawing.Size(61, 13);
+            this.txtNewName.Size = new System.Drawing.Size(119, 25);
             this.txtNewName.TabIndex = 16;
             this.txtNewName.Text = "New name:";
             // 
             // txtCurrentPhone
             // 
             this.txtCurrentPhone.AutoSize = true;
-            this.txtCurrentPhone.Location = new System.Drawing.Point(14, 190);
+            this.txtCurrentPhone.Location = new System.Drawing.Point(28, 365);
+            this.txtCurrentPhone.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtCurrentPhone.Name = "txtCurrentPhone";
-            this.txtCurrentPhone.Size = new System.Drawing.Size(115, 13);
+            this.txtCurrentPhone.Size = new System.Drawing.Size(233, 25);
             this.txtCurrentPhone.TabIndex = 17;
             this.txtCurrentPhone.Text = "Current phone number:";
             // 
             // txtNewPhone
             // 
             this.txtNewPhone.AutoSize = true;
-            this.txtNewPhone.Location = new System.Drawing.Point(237, 190);
+            this.txtNewPhone.Location = new System.Drawing.Point(474, 365);
+            this.txtNewPhone.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.txtNewPhone.Name = "txtNewPhone";
-            this.txtNewPhone.Size = new System.Drawing.Size(103, 13);
+            this.txtNewPhone.Size = new System.Drawing.Size(204, 25);
             this.txtNewPhone.TabIndex = 18;
             this.txtNewPhone.Text = "New phone number:";
             // 
             // tbxCurrentName
             // 
-            this.tbxCurrentName.Location = new System.Drawing.Point(17, 149);
+            this.tbxCurrentName.Location = new System.Drawing.Point(34, 287);
+            this.tbxCurrentName.Margin = new System.Windows.Forms.Padding(6);
             this.tbxCurrentName.Name = "tbxCurrentName";
             this.tbxCurrentName.ReadOnly = true;
-            this.tbxCurrentName.Size = new System.Drawing.Size(185, 20);
+            this.tbxCurrentName.Size = new System.Drawing.Size(366, 31);
             this.tbxCurrentName.TabIndex = 19;
             this.tbxCurrentName.TextChanged += new System.EventHandler(this.tbxCurrentName_TextChanged);
             // 
             // tbxNewName
             // 
-            this.tbxNewName.Location = new System.Drawing.Point(240, 149);
+            this.tbxNewName.Location = new System.Drawing.Point(480, 287);
+            this.tbxNewName.Margin = new System.Windows.Forms.Padding(6);
             this.tbxNewName.Name = "tbxNewName";
-            this.tbxNewName.Size = new System.Drawing.Size(185, 20);
+            this.tbxNewName.Size = new System.Drawing.Size(366, 31);
             this.tbxNewName.TabIndex = 20;
             this.tbxNewName.TextChanged += new System.EventHandler(this.validateNewName);
             // 
             // tbxNewPhone
             // 
-            this.tbxNewPhone.Location = new System.Drawing.Point(241, 206);
+            this.tbxNewPhone.Location = new System.Drawing.Point(482, 396);
+            this.tbxNewPhone.Margin = new System.Windows.Forms.Padding(6);
             this.tbxNewPhone.Name = "tbxNewPhone";
-            this.tbxNewPhone.Size = new System.Drawing.Size(185, 20);
+            this.tbxNewPhone.Size = new System.Drawing.Size(366, 31);
             this.tbxNewPhone.TabIndex = 22;
             this.tbxNewPhone.TextChanged += new System.EventHandler(this.validateNewPhone);
             // 
             // tbxCurrentPhone
             // 
-            this.tbxCurrentPhone.Location = new System.Drawing.Point(18, 206);
+            this.tbxCurrentPhone.Location = new System.Drawing.Point(36, 396);
+            this.tbxCurrentPhone.Margin = new System.Windows.Forms.Padding(6);
             this.tbxCurrentPhone.Name = "tbxCurrentPhone";
             this.tbxCurrentPhone.ReadOnly = true;
-            this.tbxCurrentPhone.Size = new System.Drawing.Size(185, 20);
+            this.tbxCurrentPhone.Size = new System.Drawing.Size(366, 31);
             this.tbxCurrentPhone.TabIndex = 21;
             // 
             // btnChangeName
             // 
-            this.btnChangeName.Location = new System.Drawing.Point(457, 142);
+            this.btnChangeName.Location = new System.Drawing.Point(914, 273);
+            this.btnChangeName.Margin = new System.Windows.Forms.Padding(6);
             this.btnChangeName.Name = "btnChangeName";
             this.btnChangeName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnChangeName.Size = new System.Drawing.Size(119, 32);
+            this.btnChangeName.Size = new System.Drawing.Size(238, 62);
             this.btnChangeName.TabIndex = 23;
             this.btnChangeName.Text = "Change name";
             this.btnChangeName.UseVisualStyleBackColor = true;
@@ -258,10 +277,11 @@
             // 
             // btnChangePhone
             // 
-            this.btnChangePhone.Location = new System.Drawing.Point(457, 194);
+            this.btnChangePhone.Location = new System.Drawing.Point(914, 373);
+            this.btnChangePhone.Margin = new System.Windows.Forms.Padding(6);
             this.btnChangePhone.Name = "btnChangePhone";
             this.btnChangePhone.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnChangePhone.Size = new System.Drawing.Size(119, 32);
+            this.btnChangePhone.Size = new System.Drawing.Size(238, 62);
             this.btnChangePhone.TabIndex = 24;
             this.btnChangePhone.Text = "Change phone";
             this.btnChangePhone.UseVisualStyleBackColor = true;
@@ -269,9 +289,9 @@
             // 
             // FormOptions
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 442);
+            this.ClientSize = new System.Drawing.Size(1568, 850);
             this.Controls.Add(this.btnChangePhone);
             this.Controls.Add(this.btnChangeName);
             this.Controls.Add(this.tbxNewPhone);
@@ -293,6 +313,7 @@
             this.Controls.Add(this.btnEditAuth);
             this.Controls.Add(this.btnEditProfile);
             this.Controls.Add(this.btnOptionsSave);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormOptions";
             this.Text = "BuzzLock Options";
             this.Load += new System.EventHandler(this.FormOptions_Load);
