e92a776c24cbefd53d128c43bae4d1a46fa7f683
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2020-04-24 16:20:41 -0400

    Add database initialization commands

--- BuzzLockGui/BuzzLockGui/Backend/Backend.cs
+++ BuzzLockGui/BuzzLockGui/Backend/Backend.cs
@@ -24,10 +24,43 @@ namespace BuzzLockGui.Backend
             = new Lazy<SQLiteConnection>(() =>
             {
                 var conn = new SQLiteConnection(CONNECTION_STRING);
-                conn.Open();
+                InitializeDatabase(conn);
                 return conn;
             });
         private static SQLiteConnection conn => _conn.Value;
+
+        private static void InitializeDatabase(SQLiteConnection conn)
+        {
+            conn.Open();
+            SQLiteCommand cmd;
+
+            cmd = new SQLiteCommand(
+                @"CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    permissionLevel INTEGER NOT NULL,
+                    phone TEXT NOT NULL,
+                    photo BLOB,
+                    cardId TEXT UNIQUE,
+                    bluetoothId INTEGER UNIQUE,
+                    bluetoothName TEXT,
+                    pin TEXT,
+                    CHECK (
+                        (cardId IS NOT NULL AND bluetoothId IS NOT NULL AND pin IS NULL)
+                        OR (cardId IS NOT NULL AND bluetoothId IS NULL AND pin IS NOT NULL)
+                        OR (cardId IS NULL AND bluetoothId IS NOT NULL AND pin IS NOT NULL)
+                    ),
+                    CHECK ((bluetoothId IS NULL) == (bluetoothName IS NULL))
+                )", conn);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand(
+                @"CREATE TABLE IF NOT EXISTS auditLog (
+                    datetime INTEGER NOT NULL,
+                    message TEXT NOT NULL
+                )", conn);
+            cmd.ExecuteNonQuery();
+        }
 
         internal static long CreateUser(
             string name,
